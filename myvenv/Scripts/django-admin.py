d6d7872ce964a172a9e0977bac16b87f53e719eb
Katsuhito <katsuhito.tonosaki@gmail.com>
2021-02-18 14:27:24 +0900

    My Django app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\katsuhito\google ドライブ\python\web開発\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
